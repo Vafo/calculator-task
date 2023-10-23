927df38e70f2a668f17a14d7480f1ba2003d511e
vafo <v.safokhujaev@student.inha.uz>
2023-10-23 10:43:52 +0500

    changed to std::allocator

--- src/util/container.h
+++ src/util/container.h
@@ -11,9 +11,6 @@ template<typename T>
 class vector_t {
 public:
     typedef int size_type;
-    typedef uint8_t raw;
-    typedef raw* raw_ptr;
-    typedef const raw* const_raw_ptr;
     typedef T* obj_ptr;
     typedef const T* const_obj_ptr;
     typedef T value_type;
@@ -22,29 +19,27 @@ public:
 
     vector_t(size_type size): 
         m_raw_ptr(
-            new raw[size * sizeof(T)]
+            allocator.allocate(size)
         ),
         m_size(size),
         m_capacity(size) {
-        obj_ptr obj = reinterpret_cast<obj_ptr>(m_raw_ptr);
         for(size_type i = 0; i < m_size; ++i)
-            new (&obj[i]) T();
+            allocator.construct(&m_raw_ptr[i]);
     }
 
     vector_t(size_type size, const T &el): 
         m_raw_ptr(
-            new raw[size * sizeof(T)]
+            allocator.allocate(size)
         ),
         m_size(size),
         m_capacity(size) {
-        obj_ptr obj = reinterpret_cast<obj_ptr>(m_raw_ptr);
         for(size_type i = 0; i < m_size; ++i)
-            new (&obj[i]) T(el);
+            allocator.construct(&m_raw_ptr[i], el);
     }
 
     vector_t(const vector_t &other):
         m_raw_ptr(
-            new raw[other.m_size * sizeof(T)]
+            allocator.allocate(other.m_size)
         ),
         m_size(other.m_size),
         m_capacity(other.m_capacity) {
@@ -55,12 +50,12 @@ public:
 
     vector_t(std::initializer_list<T> list):
         m_raw_ptr(
-            new raw[list.size() * sizeof(T)]
+            allocator.allocate(list.size())
         ),
         m_size(list.size()),
         m_capacity(list.size()) {
             copy(
-                reinterpret_cast<const_raw_ptr>(list.begin()),
+                list.begin(),
                 list.size(),
                 m_raw_ptr
             );
@@ -70,7 +65,7 @@ public:
         if(&other != this) {
             if(m_capacity < other.m_size) {
                 destroy();
-                m_raw_ptr = new raw[other.m_size * sizeof(T)];
+                m_raw_ptr = allocator.allocate(other.m_size);
                 copy(other.m_raw_ptr,
                      other.m_size,
                      m_raw_ptr);
@@ -82,18 +77,17 @@ public:
                        other.m_size,
                        m_raw_ptr);
                 
-                obj_ptr obj = reinterpret_cast<obj_ptr>(m_raw_ptr);
                 for(size_t i = other.m_size; i < m_size; ++i)
-                    obj[i].~T();
+                    allocator.destroy(&m_raw_ptr[i]);
 
                 m_size = other.m_size;
             } else { // m_size < other.m_size < m_capacity
                 assign(other.m_raw_ptr,
                        m_size,
                        m_raw_ptr);
-                copy(other.m_raw_ptr + m_size * sizeof(T),
+                copy(other.m_raw_ptr + m_size,
                      other.m_size - m_size, 
-                     m_raw_ptr + m_size * sizeof(T));
+                     m_raw_ptr + m_size);
 
                 m_size = other.m_size;
             }
@@ -118,19 +112,16 @@ public:
     value_type&
     operator[](size_type n) {
         assert(0 <= n && n < m_size);
-        obj_ptr obj = reinterpret_cast<obj_ptr>(m_raw_ptr);
-        return obj[n];
+        return m_raw_ptr[n];
     }
 
     bool
     operator==(const vector_t<T>& other) {
         if(m_size != other.m_size)
             return false;
-        
-        obj_ptr obj_1 = reinterpret_cast<obj_ptr>(m_raw_ptr);
-        obj_ptr obj_2 = reinterpret_cast<obj_ptr>(other.m_raw_ptr);
+
         for(size_t i = 0; i < m_size; ++i)
-            if(obj_1[i] != obj_2[i])
+            if(m_raw_ptr[i] != other.m_raw_ptr[i])
                 return false;
         
         return true;
@@ -138,25 +129,23 @@ public:
 
     void push_back(const value_type& el) {
         grow(1);
-        obj_ptr obj = reinterpret_cast<obj_ptr>(m_raw_ptr);
-        new (&obj[m_size])T(el);
+        allocator.construct(&m_raw_ptr[m_size], el);
         ++m_size;
     }
 
     void pop_back() {
         if(m_size > 0) {
-            obj_ptr obj = reinterpret_cast<obj_ptr>(m_raw_ptr);
-            obj[m_size].~T();
+            allocator.destroy(&m_raw_ptr[m_size-1]);
             --m_size;
         }
     }
 
     obj_ptr begin() {
-        return reinterpret_cast<obj_ptr>(m_raw_ptr);
+        return m_raw_ptr;
     }
 
     const obj_ptr begin() const {
-        return reinterpret_cast<obj_ptr>(m_raw_ptr);
+        return m_raw_ptr;
     }
     
     obj_ptr end() {
@@ -168,40 +157,37 @@ public:
     }
 
 private:
-    raw_ptr m_raw_ptr;
+    std::allocator<T> allocator;
+    obj_ptr m_raw_ptr;
     size_type m_size;
     size_type m_capacity;
 
-    void copy(const_raw_ptr src, size_type size, raw_ptr res) {
-        const_obj_ptr obj_src = reinterpret_cast<const_obj_ptr>(src);
-        obj_ptr obj_res = reinterpret_cast<obj_ptr>(res);
+    void copy(const_obj_ptr src, size_type size, obj_ptr res) {
         for (size_type i = 0; i < size; ++i)
-            new (&obj_res[i])T(obj_src[i]);
+            allocator.construct(&res[i], src[i]);
     }
 
-    void assign(raw_ptr src, size_type size, raw_ptr res) {
-        obj_ptr obj_src = reinterpret_cast<obj_ptr>(src);
-        obj_ptr obj_res = reinterpret_cast<obj_ptr>(res);
+    void assign(const_obj_ptr src, size_type size, obj_ptr res) {
         for (size_type i = 0; i < size; ++i)
-            obj_res[i] = obj_src[i];
+            res[i] = src[i];
     }
 
     void grow(size_type n) {
         if(m_size + n > m_capacity) {
             size_type new_size = std::max(m_size + n, m_capacity * 2);
-            raw_ptr tmp = new raw[new_size * sizeof(T)];
-            std::copy(m_raw_ptr, m_raw_ptr + m_size * sizeof(T), tmp);
+            obj_ptr tmp = allocator.allocate(new_size);
+            if(m_raw_ptr != NULL)
+                std::copy(m_raw_ptr, m_raw_ptr + m_size, tmp);
             m_raw_ptr = tmp;
             m_capacity = new_size;
         }
     }
 
     void destroy() {
-        obj_ptr obj = reinterpret_cast<obj_ptr>(m_raw_ptr);
         for(size_type i = 0; i < m_size; ++i)
-            obj[i].~T();
+            allocator.destroy(&m_raw_ptr[i]);
         
-        delete[] m_raw_ptr;
+        allocator.deallocate(m_raw_ptr, m_capacity);
     }
 
 };
