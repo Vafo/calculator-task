e0801441c9a1a879efbe439147b2a5a95d9edaa4
vafo <v.safokhujaev@student.inha.uz>
2023-10-24 14:09:49 +0500

    separate converter impl, added test to it

--- src/postfix.cpp
+++ src/postfix.cpp
@@ -1,46 +1,149 @@
 #include "postfix.h"
 
+#include <cstring>
+#include <numeric>
+
 #include "util/container.h"
 #include "util/shared_ptr.h"
 
 namespace postfix {
 
+namespace detail {
+
 const char *
-postfix_converter_t::to_operator(
+postfix_converter_impl_t::to_number(const char *beg, const char *end, double &out_val /*out*/) {
+    const char *cur_ptr = beg;
+
+    while(cur_ptr != end && isspace(*cur_ptr))
+        cur_ptr++;
+
+    const char *start_after_spaces = cur_ptr;
+    double res_val = 0;
+    while(cur_ptr != end && isdigit(*cur_ptr)) {
+        res_val *= 10;
+        res_val += (*cur_ptr - '0');
+        ++cur_ptr;
+    }
+
+    // return to same pos, signaling that no number was found
+    if(cur_ptr == start_after_spaces)
+        return beg;
+
+    out_val = res_val;
+    if(cur_ptr == end || cur_ptr == beg)
+        return cur_ptr;
+    
+    if(*cur_ptr != '.')
+        return cur_ptr;
+    cur_ptr++; // move 1 position, after dot
+
+    res_val = 0;
+    double dividor = 1;
+    while(cur_ptr != end && isdigit(*cur_ptr)) {
+        dividor /= 10;
+        res_val += (*cur_ptr - '0') * dividor;
+        ++cur_ptr;
+    }
+
+    out_val += res_val;
+
+    return cur_ptr;
+}
+
+const char *
+postfix_converter_impl_t::to_operator(
     const char *beg,
     const char *end,
     util::shared_ptr<token_base_t> &out_oper /*out*/
 ) {
+    assert(beg != NULL && end != NULL);
+    if(beg == end)
+        return beg;
 
-}
+    while(beg != end && isspace(*beg))
+        beg++;
 
-
-// -a + foo(b*c) * d
-// + *
-// a-bc*foo d*+
-// if 2 conseq operators, just push
-
-postfix_expr_t
-postfix_converter_t::convert(const std::string& input) {
-    char c;
-    util::stack_t< util::shared_ptr<token_base_t> > st;
-
-    bool is_number;
-    bool is_op_name;
-
-
-    for(const char *beg = input.begin().base(),
-        *end = input.end().base();
-        beg != end; 
-    ) {
-        while(isspace(*beg))
-            beg++;
-        
-        
-
+    switch(*beg) {
+        case '(':
+            // construct left-brace token
+            return beg + 1;
+        break;
+        case ')':
+            // construct right-brace token
+            return beg + 1;
+        case ',':
+            // construct comma token
+            return beg + 1;
     }
 
+    // Interval of factory_names, whose prefix equals prefix of input
+    std::string 
+        *fact_beg = factory_names.begin(),
+        *fact_end = factory_names.end(),
+        *fact_iter;
+
+    std::string op_name; // potential operation name
+    util::vector_t<int> candidates(factory_names.size()); // Ideal type would be unordered_set
+    std::iota(candidates.begin(), candidates.end(), 0);
+    int comp_val;
+    int found_idx = -1;
+
+    while(!candidates.empty()) {
+        op_name += *beg;
+        beg++;
+
+        for(
+            int *cand_beg = candidates.begin(); 
+            cand_beg != candidates.end() && beg != end;
+            cand_beg++
+        ) {
+            
+            if( (comp_val = factory_names[*cand_beg].compare(op_name)) == 0 ) {
+                found_idx = *cand_beg;
+                out_oper = factories[found_idx]->build();
+                return beg;
+            }
+            else if (comp_val < 0) {
+                candidates.erase(cand_beg);
+                --cand_beg;
+            }
+        }
+    }
+
+    // throw error, as no candidates were found
+    std::string err_msg = "postfix_converter_t::to_operator(): \
+        no operation candidate was found for " + op_name;
+    throw std::runtime_error(err_msg);
+    return beg;
 }
 
+const char*
+postfix_converter_impl_t::to_token(
+    const char* beg,
+    const char* end,
+    util::shared_ptr<token_base_t> &out_token /*out*/
+) {
+    const char *iter;
+    double num;
+    iter = to_number(beg, end, num);
+    if(iter != beg) {
+        token_number_t token_num(num);
+        out_token = util::shared_ptr<token_base_t>(token_num);
+        return iter;
+    }
+
+    util::shared_ptr<token_base_t> tmp_token;
+    iter = to_operator(beg, end, tmp_token);
+    if(iter != beg) {
+        out_token = tmp_token;
+        return iter;
+    }
+    
+    std::string err_msg = "postfix_converter_impl_t: could not convert to token" + std::string(beg);
+    throw std::runtime_error(err_msg);
+    return NULL;
+}
+
+} // namespace detail
 
 } // namespace postfix
--- src/postfix.h
+++ src/postfix.h
@@ -1,10 +1,10 @@
 #ifndef POSTFIX_H
 #define POSTFIX_H
 
-#include <string>
 #include <algorithm>
+#include <string>
 
-#include <functional>
+#include <iterator>
 
 #include "token.h"
 #include "util/container.h"
@@ -13,35 +13,37 @@
 namespace postfix
 {
 
-class postfix_converter_t;
+namespace detail {
 
-class postfix_expr_t {
-public:
-    double evaluate();
-
-private:
-    postfix_expr_t() {}
-
-    util::vector_t< util::shared_ptr<token_base_t> > postfix;
-
-    friend class postfix_converter_t; 
-};
-
-
-class postfix_converter_t {
+class postfix_converter_impl_t {
 public:
 
-    template<class... Ts>
-    postfix_converter_t(Ts... args):
-    factories({util::shared_ptr<token_factory_base_t>(&args)...}) {
+    postfix_converter_impl_t(
+        std::initializer_list<
+            util::shared_ptr<token_factory_base_t>
+        > list
+    ): factories(list) {
         std::sort(factories.begin(), factories.end(), compare_fact);
+        std::transform(
+            factories.begin(), factories.end(), 
+            std::back_inserter(factory_names), get_factory_name
+        );
     }
 
-    postfix_expr_t
-    convert(const std::string& input);
+    const char *
+    to_token(
+        const char *beg,
+        const char *end,
+        util::shared_ptr<token_base_t> &out_token /*out*/
+    );
 
-    util::vector_t< util::shared_ptr<token_factory_base_t> > factories;
 private:
+    util::vector_t< std::string > factory_names;
+    util::vector_t< util::shared_ptr<token_factory_base_t> > factories;
+    
+    // Minus sign is not supported. It is retrieved as separate operator
+    const char *
+    to_number(const char *beg, const char *end, double &out_val /*out*/);
 
     const char *
     to_operator(
@@ -54,10 +56,94 @@ private:
         util::shared_ptr<token_factory_base_t>& a,
         util::shared_ptr<token_factory_base_t>& b
     ) {
-        return a->get_name() < b->get_name();
+        return std::string(a->get_name()) < std::string(b->get_name());
     }
 
+    static std::string get_factory_name( util::shared_ptr<token_factory_base_t> &fact) {
+        return std::string(fact->get_name());
+    }
+
+#ifdef POSTFIX_TEST
+    template<class... Ts> friend class postfix_converter_impl_test;
+#endif
+
 };
+
+} // namespace detail
+
+template<class... Ts>
+class postfix_converter_t;
+
+class postfix_expr_t {
+public:
+    double evaluate();
+
+private:
+    postfix_expr_t() {}
+
+    util::vector_t< util::shared_ptr<token_base_t> > expr;
+
+    template<class... Ts> friend class postfix_converter_t; 
+};
+
+template<class... Ts>
+class postfix_converter_t {
+public:
+
+    postfix_converter_t():
+    impl({
+            util::shared_ptr<token_factory_base_t>(
+                token_factory_t<Ts>()
+            )...
+    }) { }
+
+    postfix_expr_t
+    convert(const std::string& input);
+
+private:
+    detail::postfix_converter_impl_t impl;
+
+};
+
+// -a + foo(b*c) * d
+// + *
+// a-bc*foo d*+
+// if 2 conseq operators, just push (??? not valid statement anymore?)
+
+template<class... Ts>
+postfix_expr_t
+postfix_converter_t<Ts...>::convert(const std::string& input) {
+    util::stack_t< util::shared_ptr<token_base_t> > st;
+    postfix_expr_t postfix;
+
+    const char 
+        *beg = input.begin().base(),
+        *end = input.end().base();
+    
+    // start of postfix expr
+    // postfix.expr.push_back(
+    //     util::shared_ptr<token_base_t>({token_left_parenthesis})
+    // )
+
+    util::shared_ptr<token_base_t> token;
+    while(beg != end) {
+        beg = impl.to_token(beg, end, token);
+        // put token into stack
+            // apply logic of token of emplacement
+            // if token is emplaceable, emplace it
+    }
+
+    // end of postfix expr
+    // postfix.expr.push_back(
+    //     util::shared_ptr<token_base_t>({token_right_parenthesis})
+    // )
+
+    // try to evaluate postfix expr
+        // if is executable, return it
+        // otherwise, throw exception
+
+}
+
 
 
 } // namespace postfix
